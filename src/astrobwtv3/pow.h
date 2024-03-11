83d9a03364b6fe7d7e4dac900055ace7b1871252
Tritonn204 <tritonn204@gmail.com>
2024-03-11 18:33:59 -0500

    Static linking gone again for some reason, re-added.

--- src/astrobwtv3/pow.h
+++ src/astrobwtv3/pow.h
@@ -316,29 +316,8 @@ inline void prefetch(T *data, int size, int hint) {
   const size_t prefetch_distance = 256; // Prefetch 8 cache lines ahead
   const size_t cache_line_size = 64; // Assuming a 64-byte cache line
 
-  switch(hint) {
-    case 0:
-    for (size_t i = 0; i < size; i += prefetch_distance * cache_line_size) {
-        __builtin_prefetch(&data[i], 0, 0);
-    }
-    break;
-    case 1:
-    for (size_t i = 0; i < size; i += prefetch_distance * cache_line_size) {
-        __builtin_prefetch(&data[i], 0, 1);
-    }
-    break;
-    case 2:
-    for (size_t i = 0; i < size; i += prefetch_distance * cache_line_size) {
-        __builtin_prefetch(&data[i], 0, 2);
-    }
-    break;
-    case 3:
-    for (size_t i = 0; i < size; i += prefetch_distance * cache_line_size) {
-        __builtin_prefetch(&data[i], 0, 3);
-    }
-    break;
-    default:
-    break;
+  for (size_t i = 0; i < size; i += prefetch_distance * cache_line_size) {
+      __builtin_prefetch(&data[i], 0, hint);
   }
 }
 
