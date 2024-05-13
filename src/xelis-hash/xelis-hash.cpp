428c30fb1a556f98a1a4d6b9b7161dbc14891a6f
Tritonn204 <tritonn204@gmail.com>
2024-05-13 18:27:41 -0500

    Block commented SIMD for Xelis, no performance gains left

--- src/xelis-hash/xelis-hash.cpp
+++ src/xelis-hash/xelis-hash.cpp
@@ -598,8 +598,9 @@ __attribute__((target("sse4.1"))) void stage_2(uint64_t *input, uint32_t *smallP
   // Copy slots back to the last SLOT_LENGTH elements of smallPad
   std::copy(&slots[0], &slots[XELIS_SLOT_LENGTH], &smallPad[XELIS_MEMORY_SIZE * 2 - XELIS_SLOT_LENGTH]);
 }
+*/
 
-__attribute__((target("default"))) void stage_2(uint64_t *input, uint32_t *smallPad, byte *indices, uint32_t *slots)
+void stage_2(uint64_t *input, uint32_t *smallPad, byte *indices, uint32_t *slots)
 {
   for (byte iter = 0; iter < XELIS_ITERS; ++iter)
   {
