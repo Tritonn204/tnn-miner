c13980e7236f8e6f8e324fd3c18819f86fdd3af1
Dirker Dero <dirkerdero@proton.me>
2024-06-24 01:20:17 +0000

    Add .gitattributes

--- scripts/prereqs.sh
+++ scripts/prereqs.sh
@@ -3,10 +3,16 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ROOT_DIR=$( cd -- "$( dirname -- "${SCRIPT_DIR}" )" &> /dev/null && pwd )
 
+SUDO=
+me=$(whoami)
+if [[ "$me" != "root" ]]; then
+  SUDO=sudo
+fi
 if [[ -f /etc/lsb-release ]]; then
   source /etc/lsb-release
-  sudo apt install git wget build-essential cmake clang libssl-dev libudns-dev libfmt-dev libc++-dev lld
+  $SUDO apt update
+  $SUDO apt install -y git wget build-essential cmake clang libssl-dev libudns-dev libfmt-dev libc++-dev lld libsodium-dev
   if [[ "$DISTRIB_CODENAME" == "noble" ]]; then
-    sudo apt install libboost1.83-all-dev
+    $SUDO apt install -y libboost1.83-all-dev
   fi
 fi
