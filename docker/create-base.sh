fd436c633ce081387650aeab07f930a4a7617b6b
Dirker Dero <dirkerdero@proton.me>
2024-10-29 13:03:12 -0700

    Add CI/CD for Linux ROCm builds

--- docker/create-base.sh
+++ docker/create-base.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker buildx build --push --tag dirkerdero/tnn-miner-base:latest --platform=linux/arm64,linux/amd64 -f Dockerfile.base . --builder container-builder
+docker buildx build --push --target ubuntu24-base --tag dirkerdero/tnn-miner-base:latest --platform=linux/arm64,linux/amd64 -f Dockerfile.base . --builder container-builder
+
+docker buildx build --push --target ubuntu24-rocm --tag dirkerdero/tnn-miner-base:rocm-62 --platform=linux/amd64 -f Dockerfile.base . --builder container-builder
+#docker buildx build --load --target ubuntu24-rocm --tag dirkerdero/tnn-miner-base:rocm-62 --platform=linux/amd64 -f Dockerfile.base . --builder container-builder
